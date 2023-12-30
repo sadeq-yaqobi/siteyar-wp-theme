6ac1357e0445874dd63645bad2945ebc353cd169
sadeq yaqobi <mohamadi.amir9219@gmail.com>
2023-12-30 16:40:24 +0330

    codes that are related to footer.php, header.php, index.php, style.css files were added into them. according to project structure some addresses were modified - e.g. stylesheet href, script src, img src

--- header.php
+++ header.php
@@ -1,1 +1,31 @@
-<?php
+<!DOCTYPE html>
+<html lang="fa">
+<head>
+    <meta charset="utf-8" />
+    <meta name="author" content="" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+
+    <title>LearnUp - قالب HTML دوره آنلاین و آموزش</title>
+
+    <!-- Custom CSS -->
+    <link href="<?php echo get_stylesheet_directory_uri() . '/style.css'?>" rel="stylesheet">
+
+    <!-- Custom Color Option -->
+    <link href="<?php echo get_stylesheet_directory_uri().'/assets/css/colors.css'?>" rel="stylesheet">
+
+    <!-- WordPress function for essential head elements -->
+    <?php wp_head(); ?>
+</head>
+
+<body class="red-skin rtl">
+
+<!-- ============================================================== -->
+<!-- Preloader - style you can find in spinners.css -->
+<!-- ============================================================== -->
+<div id="preloader"><div class="preloader"><span></span><span></span></div></div>
+
+
+<!-- ============================================================== -->
+<!-- Main wrapper - style you can find in pages.scss -->
+<!-- ============================================================== -->
+<div id="main-wrapper">
