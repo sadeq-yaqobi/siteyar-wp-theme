58b706fa9b634af3845b0f63eb986b2d13d18343
sadeq yaqobi <mohamadi.amir9219@gmail.com>
2024-01-01 14:49:11 +0330

    the contact.php file was created . that was divided to some parts which kept in partials/contact directory

---
+++ contact.php
@@ -0,0 +1,8 @@
+<?php /* Template Name: تماس با ما */ ?>
+<?php get_header();?>
+<?php get_template_part('partials/nav/nav','nav')?>
+<?php get_template_part('partials/contact/title','title')?>
+<?php get_template_part('partials/contact/main','main')?>
+<?php get_template_part('partials/index/newsletter','newsletter')?>
+<?php get_footer();?>
+<?php get_?>
---
+++ partials/contact/main.php
@@ -0,0 +1,107 @@
+<!-- ============================ Agency List Start ================================== -->
+<section class="bg-light">
+
+    <div class="container">
+
+        <!-- row Start -->
+        <div class="row">
+
+            <div class="col-lg-8 col-md-7">
+                <div class="prc_wrap">
+
+                    <div class="prc_wrap_header">
+                        <h4 class="property_block_title">تکمیل فرم درخواست</h4>
+                    </div>
+
+                    <div class="prc_wrap-body">
+                        <div class="row">
+                            <div class="col-lg-6 col-md-12">
+                                <div class="form-group">
+                                    <label>نام کامل</label>
+                                    <input type="text" class="form-control simple">
+                                </div>
+                            </div>
+                            <div class="col-lg-6 col-md-12">
+                                <div class="form-group">
+                                    <label>ایمیل</label>
+                                    <input type="email" class="form-control simple">
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label>عنوان</label>
+                            <input type="text" class="form-control simple">
+                        </div>
+
+                        <div class="form-group">
+                            <label>پیام</label>
+                            <textarea class="form-control simple"></textarea>
+                        </div>
+
+                        <div class="form-group">
+                            <button class="btn btn-theme" type="submit">ارسال درخواست</button>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+
+            <div class="col-lg-4 col-md-5">
+
+                <div class="prc_wrap">
+
+                    <div class="prc_wrap_header">
+                        <h4 class="property_block_title">راه های ارتباطی</h4>
+                    </div>
+
+                    <div class="prc_wrap-body">
+                        <div class="contact-info">
+
+                            <h2>در تماس باشید</h2>
+                            <p>برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد. </p>
+
+                            <div class="cn-info-detail">
+                                <div class="cn-info-icon">
+                                    <i class="ti-home"></i>
+                                </div>
+                                <div class="cn-info-content">
+                                    <h4 class="cn-info-title">آدرس ما</h4>
+                                    ایران، تهران <br>خیابان سعادت آباد،<br> خیابان کاج شمال غربی
+                                </div>
+                            </div>
+
+                            <div class="cn-info-detail">
+                                <div class="cn-info-icon">
+                                    <i class="ti-email"></i>
+                                </div>
+                                <div class="cn-info-content">
+                                    <h4 class="cn-info-title">پست الکترونیکی</h4>
+                                    support@example.com<br>example@gmail.com
+                                </div>
+                            </div>
+
+                            <div class="cn-info-detail">
+                                <div class="cn-info-icon">
+                                    <i class="ti-mobile"></i>
+                                </div>
+                                <div class="cn-info-content">
+                                    <h4 class="cn-info-title">شماره تماس</h4>
+                                    (41) 123 521 458<br>+91 235 548 7548
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+        <!-- /row -->
+
+    </div>
+
+</section>
+<!-- ============================ Agency List End ================================== -->
