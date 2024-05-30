3a42e76797105f1c407c571e406284df1ee14435
sadeq yaqobi <mohamadi.amir9219@gmail.com>
2024-05-30 23:20:57 +0330

    UI DebugHelper class was improved

--- helper-class/DebugHelper.php
+++ helper-class/DebugHelper.php
@@ -14,8 +14,55 @@ class DebugHelper
      */
     public static function dump($input, bool $collapse = false)
     {
+        // Include external CSS and JS for modern UI
+        echo '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">';
+        echo '<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>';
+        echo '<style>
+            .debug-container {
+                direction: ltr;
+                padding: 20px;
+                font-size: 14px;
+                background-color: #f0f2f5;
+                border: 1px solid #dcdcdc;
+                border-radius: 8px;
+                font-family: Arial, sans-serif;
+            }
+            .debug-key {
+                color: #495057;
+            }
+            .debug-type {
+                color: #adb5bd;
+                font-weight: lighter;
+                font-size: 13px;
+            }
+            .debug-string {
+                color: #d6336c;
+            }
+            .debug-float {
+                color: #20c997;
+            }
+            .debug-integer {
+                color: #fd7e14;
+            }
+            .debug-boolean {
+                color: #0d6efd;
+            }
+            .debug-toggle {
+                color: #0d6efd;
+                cursor: pointer;
+                text-decoration: none;
+            }
+            .debug-toggle .fas {
+                font-size: 12px;
+            }
+            .nested {
+                margin-left: 20px;
+                display: none;
+            }
+        </style>';
+
         // Start debug output
-        echo '<div style="direction: ltr; padding: 35px;font-size: 16px;background-color: #F4F5F7">';
+        echo '<div class="debug-container">';
 
         // Recursive function to process and output debug information
         $recursive = function ($data, $level = 0) use (&$recursive, $collapse) {
@@ -27,39 +74,39 @@ class DebugHelper
             if (!$isTerminal && $level == 0 && !defined("DUMP_DEBUG_SCRIPT")) {
                 define("DUMP_DEBUG_SCRIPT", true); // Prevent multiple script definitions
 
-                echo '<script language="Javascript">function toggleDisplay(id) {';
-                echo 'var state = document.getElementById("container"+id).style.display;';
-                echo 'document.getElementById("container"+id).style.display = state == "inline" ? "none" : "inline";';
-                echo 'document.getElementById("plus"+id).style.display = state == "inline" ? "inline" : "none";';
-                echo '}</script>' . PHP_EOL;
+                echo '<script>
+                        $(document).ready(function() {
+                            $(".debug-toggle").click(function() {
+                                var target = $(this).data("target");
+                                $("#" + target).slideToggle();
+                                $(this).find(".fas").toggleClass("fa-plus-square fa-minus-square");
+                            });
+                        });
+                      </script>';
             }
 
             // Determine type and format data accordingly
             $type = !is_string($data) && is_callable($data) ? "Callable" : ucfirst(gettype($data));
+            $type_class = strtolower($type);
             $type_data = null;
-            $type_color = null;
             $type_length = null;
 
             switch ($type) {
                 case "String":
-                    $type_color = "#F6511D";
                     $type_length = strlen($data);
                     $type_data = "\"" . htmlentities($data) . "\"";
                     break;
                 case "Double":
                 case "Float":
                     $type = "Float";
-                    $type_color = "#0099c5";
                     $type_length = strlen($data);
                     $type_data = htmlentities($data);
                     break;
                 case "Integer":
-                    $type_color = "#93032E";
                     $type_length = strlen($data);
                     $type_data = htmlentities($data);
                     break;
                 case "Boolean":
-                    $type_color = "#00ABF5 ";
                     $type_length = strlen($data);
                     $type_data = $data ? "TRUE" : "FALSE";
                     break;
@@ -90,17 +137,16 @@ class DebugHelper
                         } else {
                             $id = substr(md5(rand() . ":" . $key . ":" . $level), 0, 8);
 
-                            echo "<a href=\"javascript:toggleDisplay('" . $id . "');\" style=\"text-decoration:none\">";
-                            echo "<span style='color:#b7b7b8;font-weight: lighter;font-size: 13px;'>" . $type . ($type_length !== null ? "(" . $type_length . ")" : "") . "</span>";
+                            echo "<a href=\"javascript:void(0);\" class=\"debug-toggle\" data-target=\"container" . $id . "\">";
+                            echo "<span class='fas " . ($collapse ? "fa-plus-square" : "fa-minus-square") . "'></span>";
+                            echo "<span class='debug-type'>" . $type . ($type_length !== null ? "(" . $type_length . ")" : "") . "</span>";
                             echo "</a>";
-                            echo "<span id=\"plus" . $id . "\" style=\"display: " . ($collapse ? "inline" : "none") . ";\">&nbsp;&#10549;</span>";
-                            echo "<div id=\"container" . $id . "\" style=\"display: " . ($collapse ? "" : "inline") . ";\">";
-                            echo "<br />";
+                            echo "<div id=\"container" . $id . "\" class=\"nested\" style=\"display: " . ($collapse ? "none" : "block") . ";\">";
                         }
 
                         // Indentation for nested levels
                         for ($i = 0; $i <= $level; $i++) {
-                            echo $isTerminal ? "|    " : "<span style='color:#b7b7b8'>|</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
+                            echo $isTerminal ? "|    " : "<span class='debug-type'>|</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
                         }
 
                         echo $isTerminal ? "\n" : "<br />";
@@ -108,11 +154,11 @@ class DebugHelper
 
                     // Indentation for nested levels
                     for ($i = 0; $i <= $level; $i++) {
-                        echo $isTerminal ? "|    " : "<span style='color:#b7b7b8'>|</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
+                        echo $isTerminal ? "|    " : "<span class='debug-type'>|</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
                     }
 
                     // Output key-value pairs
-                    echo $isTerminal ? "[" . $key . "] => " : "<span style='color:#070600'>[" . $key . "]&nbsp;=>&nbsp;</span>";
+                    echo $isTerminal ? "[" . $key . "] => " : "<span class='debug-key'>[" . $key . "]&nbsp;=>&nbsp;</span>";
                     call_user_func($recursive, $value, $level + 1);
                 }
 
@@ -124,10 +170,10 @@ class DebugHelper
                         $value = $property->getValue($data);
 
                         for ($i = 0; $i <= $level; $i++) {
-                            echo $isTerminal ? "|    " : "<span style='color:#b7b7b8'>|</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
+                            echo $isTerminal ? "|    " : "<span class='debug-type'>|</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
                         }
 
-                        echo $isTerminal ? "[" . $key . "] => " : "<span style='color:#070600'>[" . $key . "]&nbsp;=>&nbsp;</span>";
+                        echo $isTerminal ? "[" . $key . "] => " : "<span class='debug-key'>[" . $key . "]&nbsp;=>&nbsp;</span>";
                         call_user_func($recursive, $value, $level + 1);
                     }
                 }
@@ -135,7 +181,7 @@ class DebugHelper
                 // Close divs and output if non-empty
                 if ($notEmpty) {
                     for ($i = 0; $i <= $level; $i++) {
-                        echo $isTerminal ? "|    " : "<span style='color:#b7b7b8'>|</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
+                        echo $isTerminal ? "|    " : "<span class='debug-type'>|</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
                     }
 
                     if (!$isTerminal) {
@@ -144,16 +190,16 @@ class DebugHelper
                 } else {
                     echo $isTerminal ?
                         $type . ($type_length !== null ? "(" . $type_length . ")" : "") . "  " :
-                        "<span style='color:#b7b7b8;font-weight: lighter;font-size: 13px;'>" . $type . ($type_length !== null ? "(" . $type_length . ")" : "") . "</span>&nbsp;&nbsp;";
+                        "<span class='debug-type'>" . $type . ($type_length !== null ? "(" . $type_length . ")" : "") . "</span>&nbsp;&nbsp;";
                 }
             } else {
                 // Output non-array and non-object types
                 echo $isTerminal ?
                     $type . ($type_length !== null ? "(" . $type_length . ")" : "") . "  " :
-                    "<span style='color:#b7b7b8;font-weight: lighter;font-size: 13px;'>" . $type . ($type_length !== null ? "(" . $type_length . ")" : "") . "</span>&nbsp;&nbsp;";
+                    "<span class='debug-type'>" . $type . ($type_length !== null ? "(" . $type_length . ")" : "") . "</span>&nbsp;&nbsp;";
 
                 if ($type_data != null) {
-                    echo $isTerminal ? $type_data : "<span style='color:" . $type_color . "'>" . $type_data . "</span>";
+                    echo $isTerminal ? $type_data : "<span class='debug-" . $type_class . "'>" . $type_data . "</span>";
                 }
             }
 
